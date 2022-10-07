334d4ea9effec8dd47308d19b30313537047ea6e
Mikayla Grady <mikayla.griffiths@snhu.edu>
2022-10-06 23:06:44 -0600

    MongoDB

---
+++ seedgoosesrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  modelBaseDirectory: "app_server/database/models", // model directory name
+  models: ["*.js", "!db.js"], // model matches
+  data: "data", // data directory
+  db: "mongodb"//localhost:27017/travlr" // db connection url
+  };
