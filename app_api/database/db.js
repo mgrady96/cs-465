65f5c6fbadd35ff38068fb746eb1467e2c751118
Mikayla Grady <mikayla.griffiths@snhu.edu>
2022-10-13 18:08:50 -0600

    Updates

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -50,4 +50,4 @@ process.on('SIGTERM', () => {
 });
 
 
-require('./Trips');
+require('./models/travlr');
--- seedgoosesrc.js
+++ seedgoosesrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  modelBaseDirectory: "app_server/database/models", // model directory name
+  modelBaseDirectory: "app_api/database/models", // model directory name
   models: ["*.js", "!db.js"], // model matches
   data: "data", // data directory
   db: "mongodb"//localhost:27017/travlr" // db connection url
